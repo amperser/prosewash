3405253f425080e77af2935faf85d730a3af06c1
M Pacer <mpacer@berkeley.edu>
2017-10-30 10:01:42 -0700

    isolatable function for us to use interactively

--- quickwash.py
+++ quickwash.py
@@ -2,17 +2,18 @@ import json
 import proselint
 import sys
 
+def error_str_to_json(err_string):
+    return json.loads(
+        proselint.tools.errors_to_json(
+            proselint.tools.lint(err_string)
+            )
+        )
 
 def main(text=None):
     if text is None:
         with open(sys.argv[1], "r") as f:
             text = f.read()
-    json_dict = json.loads(
-        proselint.tools.errors_to_json(
-            proselint.tools.lint(text)
-            )
-        )
-    print(json_dict)
+    print(error_str_to_json(text))
 
 if __name__ == "__main__":
     main()

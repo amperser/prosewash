5ebed5981edc284fe878b61fd5452e108772b2f4
Jordan Suchow <suchow@post.harvard.edu>
2017-10-30 13:27:03 -0400

    Get sentence context

--- quickwash.py
+++ quickwash.py
@@ -1,6 +1,12 @@
 import json
-import proselint
 import sys
+
+import spacy
+
+import proselint
+
+nlp = spacy.load('en')
+
 
 def error_str_to_json(err_string):
     return json.loads(
@@ -9,11 +15,27 @@ def error_str_to_json(err_string):
             )
         )
 
+
+def sentence_context(text, position):
+    """The sentence in the text that includes the given position."""
+    doc = nlp(text)
+    print(text)
+    print(type(position))
+    counter = 0
+    for sentence in doc.sents:
+        counter += len(sentence)
+        if counter >= position:
+            return sentence
+
+
 def main(text=None):
     if text is None:
         with open(sys.argv[1], "r") as f:
             text = f.read()
-    print(error_str_to_json(text))
+    errors = error_str_to_json(text)['data']['errors']
+    for error in errors:
+        print(sentence_context(text, error['start']))
+
 
 if __name__ == "__main__":
     main()

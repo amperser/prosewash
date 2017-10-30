bd194ed010089d5aa2c6aef1db8c58afd47353bb
M Pacer <mpacer@berkeley.edu>
2017-10-30 09:51:44 -0700

    first quick wash script

---
+++ quickwash.py
@@ -0,0 +1,17 @@
+import json
+import proselint
+import sys
+
+
+def main():
+    with open(sys.argv[1], "r") as f:
+        text = f.read()
+    json_dict = json.loads(
+        proselint.tools.errors_to_json(
+            proselint.tools.lint(text)
+            )
+        )
+    print(json_dict)
+
+if __name__ == "__main__":
+    main()

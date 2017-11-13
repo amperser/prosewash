fc8fb7dc8fd757a33e823a91bd80f2490de8ba20
M Pacer <mpacer@berkeley.edu>
2017-11-13 12:34:35 -0800

    basic local webapp

--- app.py
+++ app.py
@@ -1,7 +1,44 @@
+import os
+import uuid
+
+from datetime import datetime
+
 from flask import Flask, request
-# from quickwash import
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.dialects.postgresql import UUID
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+
+db = SQLAlchemy(app)
+
+
+class Stain(db.Model):
+    __tablename__ = "stain"
+    id = db.Column(
+        UUID,
+        primary_key=True,
+        nullable=False,
+        default=str(uuid.uuid4())
+        )
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    message = db.Column(db.Text)
+    context = db.Column(db.Text)
+    valid = db.Column(db.Boolean)
+    document_id = db.Column(UUID, db.ForeignKey('document.id'))
+
+class Document(db.Model):
+    __tablename__ = "document"
+    id = db.Column(
+        UUID,
+        primary_key=True,
+        nullable=False,
+        default=str(uuid.uuid4())
+        )
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    text = db.Column(db.Text)
+    stains = db.relationship(Stain, backref="document")
+
 
 @app.route('/')
 def index():
@@ -9,12 +46,16 @@ def index():
 
 @app.route('/document', methods=["POST"])
 def document_post():
-    app.logger.info(request.values["text"])
-    return ("post job!", 200)
+    document = Document()
+    document.text = request.values["text"]
+    db.session.add(document)
+    db.session.commit()
+    return (document.id, 200)
 
-@app.route('/document', methods=["GET"])
-def document_get():
-    return ('you got it', 200)
+@app.route('/document/<uuid:id>', methods=["GET"])
+def document_get(id):
+    document = Document.query.filter_by(id=str(id)).one()
+    return (document.text, 200)
 
 def main():
     app.run(debug=True, use_reloader=True)
--- requester.py
+++ requester.py
@@ -1,6 +1,15 @@
 import requests
-
+import time
 contents = {
     "text": "Quick brown fox jumped over the lazy dog."
 }
-requests.post("http://0.0.0.0:5000/document", data=contents)
+base_url = "http://0.0.0.0:5000/document"
+
+response = requests.post(base_url, data=contents)
+# response = requests.post("http://prosewash.herokuapp.com/document", data=contents)
+# print(response.content)
+time.sleep(.5)
+second_url = base_url+"/"+response.content.decode('utf-8')
+print(second_url)
+response2 = requests.get(second_url)
+print(response2.content)
